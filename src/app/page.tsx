5e6051043798a0d77690444f4fb1ae0c66565bb1
Suraj Thapa <surajthapa@Surajs-MacBook-Pro.local>
2025-01-20 17:10:33 +0545

    Other tracker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 'use client'
 import axios from "axios";
 import {useEffect} from "react";
+import {useSearchParams} from "next/navigation";
 
 
 export default function Home() {
 
+    const searchParams = useSearchParams()
+
     const init = async () => {
         try {
-            await axios.post(process.env.NEXT_APP_LOG_SERVER_API_URL ?? '', {}, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'x-api-key': process.env.NEXT_APP_LOG_SERVER_API_KEY
+            const type = searchParams.get('type') ?? 'fb'
+            const url = `${process.env.NEXT_APP_LOG_SERVER_API_URL ?? ''}?type=${type}`
+            await axios.post(url, {}, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'x-api-key': process.env.NEXT_APP_LOG_SERVER_API_KEY
+                    }
                 }
-            })
+            )
         } catch (exception) {
             console.log(exception)
         } finally {
